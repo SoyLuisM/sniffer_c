67f915691189cd6967b7908ae666e92f999b93e7
Luis Martinez <s_k_ap3@hotmail.com>
2021-09-04 02:19:36 -0500

    dividiendo el resultado en archivos diferentes segun su protocolo

--- proc_dat.h
+++ proc_dat.h
@@ -4,15 +4,44 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include <linux/if_ether.h>
 
 void *procesar_datos(void *datos){
+    FILE *fichero,*fichero2,*fichero3;
+    fichero=fopen("sniffer.log","w");
+    fichero2=fopen("sniffer2.log","w");
+    fichero3=fopen("sniffer3.log","w");
+    
     struct parametros *param;
     int tuberia, long_dat;
+    struct ethhdr *eth;
+    char buffer[MAX_LINES];
     tuberia = open("/tmp/mi_fifo",O_RDONLY);
 
     long_dat=read(tuberia, &param, sizeof(struct parametros *));
 
-    printf("hola soy el procesador de datos %d %s %d\n", long_dat, param->interfaz, param->n_paquetes);
+    for(int i = 0; i<param->n_paquetes;i++ ){
+        long_dat=read(tuberia, buffer, MAX_LINES);
+        eth = (struct ethhdr *)buffer;
+        if(htons(eth->h_proto) ==0x0800){
+            fprintf(fichero,"%02X.%02x.%02x.%02x.%02x.%02x\t",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
+            fprintf(fichero,"%02x.%02x.%02x.%02x.%02x.%02x\t",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
+            fprintf(fichero,"%x\n",htons(eth->h_proto));
+        }else if(htons(eth->h_proto) ==0x86DD){
+            fprintf(fichero2,"%02X.%02x.%02x.%02x.%02x.%02x\t",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
+            fprintf(fichero2,"%02x.%02x.%02x.%02x.%02x.%02x\t",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
+            fprintf(fichero2,"%x\n",htons(eth->h_proto));
+        }else if(htons(eth->h_proto) ==0x0806){
+            fprintf(fichero3,"%02X.%02x.%02x.%02x.%02x.%02x\t",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
+            fprintf(fichero3,"%02x.%02x.%02x.%02x.%02x.%02x\t",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
+            fprintf(fichero3,"%x\n",htons(eth->h_proto));
+        }else{
+            printf("%02X.%02x.%02x.%02x.%02x.%02x\t",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
+            printf("%02x.%02x.%02x.%02x.%02x.%02x\t",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
+            printf("%x\n",htons(eth->h_proto));
+        }
+    }
+    
     close(tuberia);
     pthread_exit(0);
 }
--- snoopy.h
+++ snoopy.h
@@ -4,5 +4,8 @@ struct parametros{
     char *interfaz;
     int n_paquetes;
 };
+/*el tamaño maximo de la trama ethernet es 1518 bytes
+dejo este valor nomas por que si*/
+#define MAX_LINES 65536 
 //#include  ″snoopy.c″
 #endif
